066a5abed023cff9011176a054f1bfc6c3f3c1bb
mariadlcruzz0-0 <mariadlcruzz0.0@gmail.com>
2022-09-20 18:14:23 -0700

    fixed my startIndex Variable and my endIndex variable. Added and information of each student inside the if statement(showPage function)

--- js/script.js
+++ js/script.js
@@ -16,25 +16,33 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 function showPage(list, page) {
-startIndex = `(${page} * list.length) - list.length`;
-endIndex = `(${page} * list.length)`;
-const studentList = document.getElementsByClassName('student-list');
-
+startIndex = (page * 9) - 9;
+endIndex = page * 9;
+const studentList = document.querySelector('.student-list');
+console.log(studentList);
 studentList.innerHTML = '';
 
 for (let i = 0; i < list.length; i++ ) {
-   if(i >= startIndex & i <= endIndex ){
-         
+   [console.log(list[i])]
+   if(i >= startIndex && i <= endIndex){
+      
+   let studentInfo = `  <li class="student-item cf">
+                        <div class="student-details">
+                        <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
+                        <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                        <span class="email">${list[i].email}</span>
+                        </div>
+                        <div class="joined-details">
+                        <span class="date">Joined ${list[i].registered.date}</span>
+                        </div>
+                     </li>`;
+                     studentList.insertAdjacentHTML('beforeend', studentInfo)
       }
    }
 }
-
+showPage(data, 1);
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
-
-
-// Call functions

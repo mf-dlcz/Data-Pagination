8b95bb772be6f483acde89846ea3d89d27d5f092
Maria Delacruz <mar23iaa@gmail.com>
2024-02-05 15:53:24 -0800

    added README and deleted comments

---
+++ README.md
@@ -0,0 +1,54 @@
+# Student Directory
+
+## Table of Contents
+
+- [Student Directory](#student-directory)
+  - [Table of Contents](#table-of-contents)
+  - [Project Overview](#project-overview)
+  - [Demo](#demo)
+  - [Technologies Used](#technologies-used)
+  - [How to Use](#how-to-use)
+
+## Project Overview
+
+This project showcases my city.
+
+![website screenshot](/examples/example-meets.png)
+
+## Demo
+
+- Live demo: [Student Directory]()
+
+## Technologies Used
+
+The project utilizes the following technologies and features:
+
+- HTML
+- CSS
+- JavaScript
+
+## How to Use
+
+1. **Clone the repository**:
+
+First, you'll need to clone this repository to your local machine. Open your terminal and run the following command:
+
+```git
+git clone https://github.com/mf-dlcz/Data-Pagination.git
+```
+
+2. **Navigate to the Project Folder**:
+
+Change your working directory to the newly cloned project folder:
+
+```git
+ cd js
+```
+
+3. **Run the project**:
+
+```git
+node script.js
+```
+
+This command will launch a development server, and you'll be able to view the website locally in your web browser.
--- js/script.js
+++ js/script.js
@@ -1,76 +1,54 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
 function showPage(list, page) {
-startIndex = (page * 9) - 9;
-endIndex = page * 9;
-const studentList = document.querySelector('.student-list');
-studentList.innerHTML = '';
+   startIndex = (page * 9) - 9;
+   endIndex = page * 9;
+   const studentList = document.querySelector('.student-list');
+      studentList.innerHTML = '';
 
-for (let i = 0; i < list.length; i++ ) {
-   if(i >= startIndex && i < endIndex){
-   
-   //Diplays this information for each student
-   let studentInfo = `  <li class="student-item cf">
-                        <div class="student-details">
-                        <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
-                        <h3>${list[i].name.first} ${list[i].name.last}</h3>
-                        <span class="email">${list[i].email}</span>
-                        </div>
-                        <div class="joined-details">
-                        <span class="date">Joined ${list[i].registered.date}</span>
-                        </div>
-                     </li>`;
-                     studentList.insertAdjacentHTML('beforeend', studentInfo)
+   for (let i = 0; i < list.length; i++ ) {
+      if(i >= startIndex && i < endIndex){
+      
+      //Diplays this information for each student
+      let studentInfo = `  <li class="student-item cf">
+                           <div class="student-details">
+                           <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
+                           <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                           <span class="email">${list[i].email}</span>
+                           </div>
+                           <div class="joined-details">
+                           <span class="date">Joined ${list[i].registered.date}</span>
+                           </div>
+                        </li>`;
+                        studentList.insertAdjacentHTML('beforeend', studentInfo)
+         }
       }
-   }
 }
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
 
 function addPagination(list) {
-const numOfPages = Math.ceil( list.length / 9 );
-const linkList = document.querySelector('ul.link-list');
-linkList.innerHTML = '';
+   const numOfPages = Math.ceil( list.length / 9 );
+   const linkList = document.querySelector('ul.link-list');
+      linkList.innerHTML = '';
 
-//Loops through the number of pages
-for(let i = 1; i <= numOfPages; i++) {
-   const button = `<li>
-                  <button type='button'>${i}</button>
-                  </li>`;
-   linkList.insertAdjacentHTML('beforeend', button);
-   //console.log(button)                  
-   }
-const firstButton = document.querySelector('.link-list button');
-firstButton.className = 'active';
+   //Loops through the number of pages
+   for(let i = 1; i <= numOfPages; i++) {
+      const button = `<li>
+                     <button type='button'>${i}</button>
+                     </li>`;
+      linkList.insertAdjacentHTML('beforeend', button);                 
+      }
+   const firstButton = document.querySelector('.link-list button');
+   firstButton.className = 'active';
 
-//Event Listener checkes for user clicks
-linkList.addEventListener('click', (e) => {
-   if( e.target.tagName === 'BUTTON' ) {
-      let activeClass = document.querySelector('.active');
-      activeClass.className = '';
-      e.target.className = 'active';
-      showPage(list, e.target.textContent);
-   }
-})
+   //Event Listener checkes for user clicks
+   linkList.addEventListener('click', (e) => {
+      if( e.target.tagName === 'BUTTON' ) {
+         let activeClass = document.querySelector('.active');
+         activeClass.className = '';
+         e.target.className = 'active';
+         showPage(list, e.target.textContent);
+      }
+   })
+}
 
 //Calling my functions
-}
 showPage(data, 1);
 addPagination(data);
